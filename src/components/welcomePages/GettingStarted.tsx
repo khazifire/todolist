0447a0a042d9452cf3be966a51966cb0d2c0428c
Dan <khazifire@gmail.com>
2020-12-02 20:03:39 +0700

    add slides to welcome page

--- src/components/welcomePages/GettingStarted.tsx
+++ src/components/welcomePages/GettingStarted.tsx
@@ -10,7 +10,7 @@ import { arrowForward } from 'ionicons/icons';
 // Optional parameters to pass to the swiper instance.
 // See http://idangero.us/swiper/api/ for valid options.
 const slideOpts = {
-  initialSlide: 1,
+  initialSlide: 0,
   speed: 400
 };
 
@@ -34,13 +34,51 @@ const SlidesExample: React.FC = () => {
         <div className="slide">
           <img src="./slide-2.png"/>
           <h2><b>Getting started</b> </h2>
-          <p>put something here then like to register page</p>
-          <IonButton fill="clear">Continue <IonIcon slot="end" icon={arrowForward} ></IonIcon></IonButton>
+          <p>In order to use TRACKT you need to create an account, so that your record can be saved</p>
+          <IonButton fill="clear">NEXT <IonIcon slot="end" icon={arrowForward} ></IonIcon></IonButton>
           </div> 
         </IonSlide>
 
        
+        <IonSlide>
+        <div className="slide">
+          <img src="./slide-2.png"/>
+          <h2><b>1</b> </h2>
+          <p>After signing up, you will be automatically be redirect to the homepage.</p>
+          <IonButton fill="clear">NEXT<IonIcon slot="end" icon={arrowForward} ></IonIcon></IonButton>
+          </div> 
+        </IonSlide>
 
+        <IonSlide>
+        <div className="slide">
+          <img src="./slide-2.png"/>
+          <h2><b>2</b> </h2>
+          <p>To use the tracker, click on the plus icon, on the bottom right, or the tab "Tracker"</p>
+          <p>Fill up all the empty field while on the tracker page</p>
+          <IonButton fill="clear">NEXT<IonIcon slot="end" icon={arrowForward} ></IonIcon></IonButton>
+          </div> 
+        </IonSlide>
+        
+
+        
+        <IonSlide>
+        <div className="slide">
+          <img src="./slide-2.png"/>
+          <h2><b>3</b> </h2>
+          <p>Press START, to initiate the timer, and STOP to terminate the timer thus saving the timer spent on that activites </p>
+          <IonButton fill="clear">NEXT<IonIcon slot="end" icon={arrowForward} ></IonIcon></IonButton>
+          </div> 
+        </IonSlide>
+
+        
+        <IonSlide>
+        <div className="slide">
+          <img src="./slide-2.png"/>
+          <h2><b>4</b> </h2>
+          <p>The HOME page,diaplays all the record of the activites you kept track of. The PROGRESS page, displays a grpahical summary of the activites you did</p>
+          <IonButton fill="clear" routerLink="/my/home">Lets dive in <IonIcon slot="end" icon={arrowForward} ></IonIcon></IonButton>
+          </div> 
+        </IonSlide>
       </IonSlides>
     </IonContent>
   </IonPage>
