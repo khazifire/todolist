d118a727525c20182ca1db1eb658697103c1d089
dan <Student@IT224-09.apiu.edu>
2020-12-10 22:18:12 +0700

    updated settings page and timer

--- src/components/TimeTrackerPages/TimeTracker.tsx
+++ src/components/TimeTrackerPages/TimeTracker.tsx
@@ -14,15 +14,15 @@ import {
   IonPopover,
   IonInput,
   IonItem,
-  IonDatetime,
 } from "@ionic/react";
 import React, { useState, useEffect } from "react";
 import Timer from "react-compound-timer";
 import { useHistory } from "react-router-dom";
-import { auth } from "../../firebase";
 import { firestore } from "../../firebase";
 import { useAuth } from "../../auth";
 import ".//tracker.css"
+
+/* import ".//clock.css" */
 
 import moment from "moment";
 
@@ -32,11 +32,8 @@ const SettingsPage: React.FC = () => {
   const [description, setDescription] = useState("");
   const history = useHistory();
   const [showPopover, setShowPopover] = useState(false);
-
   const [AmountOfTimeWorked, settimeworked] = useState<any>("");
-  // const [timeInMinutes, settimeInMinutes] = useState<any>("");
-  // const [timeInHours, settimeInHours] = useState<any>("");
-
+  
   const handleTimeIn = () => {
     const now = moment();
     console.log("Time In: ", now);
@@ -47,26 +44,12 @@ const SettingsPage: React.FC = () => {
     const now = moment();
     console.log("Time out: ", now);
 
-
     const time1 = moment(timeIn);
     const timeDiff = now.diff(time1);
     const inseconds = (timeDiff / 1000);
     console.log(timeDiff / 1000, "Second");
     settimeworked(inseconds);
-
-
-
   };
-
-  // const convertime =() =>{
-  //   if (timeworked>10){
-  //     settimeInMinutes(timeworked/15)
-  //     console.log(timeInMinutes, "Second");
-  //   } else if(timeInMinutes>2)
-  //   settimeInHours(timeInMinutes/3)
-  //   console.log(timeInHours, "Second");
-
-  // }
 
   const workedTimePopUp = () => {
     handleSaveTime();
@@ -74,17 +57,6 @@ const SettingsPage: React.FC = () => {
     setShowPopover(false);
 
   };
-
-  // function StoreTimeInFirestore() {
-  //   firestore.collection("users").doc(userId).collection("entries").add({
-  //     description: description,
-  //     title: description,
-  //     workedTime: timeworked,
-  //     date: new Date(),
-  //   });
-  //   history.goBack();
-  // }
-
 
   var [currentdate, setDate] = useState(new Date());
   useEffect(() => {
@@ -96,8 +68,6 @@ const SettingsPage: React.FC = () => {
 
   const [title, setTitle] = useState('');
   const [date, setdate] = useState(new Date().toISOString());
-
-
   const StoreTimeInFirestore = async () => {
     const entriesRef = firestore.collection('users').doc(userId)
       .collection('TracktUserRecords');
@@ -107,27 +77,24 @@ const SettingsPage: React.FC = () => {
     history.goBack();
   }
 
- 
-  
- 
-
   return (
     <IonPage>
 
 
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>TRACKT</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonCard>
-          <IonCardHeader>
-            <IonText></IonText>
-          </IonCardHeader>
       
+      </IonHeader>
+      <IonContent className="ion-text-center">
+        <IonCard>
+        
+          <IonCardHeader className="clock">
           <IonLabel className='currentDateLabel'> {currentdate.toLocaleDateString()}</IonLabel> <br></br>
-          <IonLabel className='currentTimeLabel'> {currentdate.toLocaleTimeString()}</IonLabel> 
+          <IonLabel >day here</IonLabel>
+          {/* <IonLabel className='currentTimeLabel'> {currentdate.toLocaleTimeString()}</IonLabel>  */}
+          </IonCardHeader>
+          
+         
+      
           </IonCard>
         <IonCard>
         <IonItem>
@@ -162,7 +129,7 @@ const SettingsPage: React.FC = () => {
             )}
           </Timer>
         </IonCard>
-
+      
         <IonPopover isOpen={showPopover} cssClass="conainer-of-Pop-Ups" onDidDismiss={(e) => workedTimePopUp()}>
           <p className="centerText">Congratulation!</p>
           <p className="centerText">You spent {AmountOfTimeWorked} seconds working </p>
@@ -170,7 +137,7 @@ const SettingsPage: React.FC = () => {
         </IonPopover>
 
       </IonContent>
-
+    
     </IonPage>
   );
 };
--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -6,23 +6,120 @@ import {
   IonToolbar,
   IonPage,
   IonButton,
+  IonItem,
+  IonLabel,
+  IonToggle,
   IonCard,
-  IonCardHeader,
-  IonCardContent,
-  IonText
-
+  IonList,
+  IonModal,
+  IonText,
 } from '@ionic/react';
 import React, { useState } from 'react';
 import { auth } from '../firebase';
 
-
+const toggleDarkModeHandler = () => {
+  document.body.classList.toggle("dark");
+};
 
 const SettingsPage: React.FC = () => {
-
+  
+  /* https://undraw.co/search */
 
   return (
-  
-<IonPage>
+    <IonPage>
+      <IonHeader translucent>
+        <IonToolbar>
+          <IonTitle>
+            {" "}
+            <div className="ion-text-center"> Settings</div>
+          </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-text-center" fullscreen>
+        <IonCard>
+
+          <img
+            height="150 px"
+            src={"/assets/settings.svg"}
+            alt="setting image"
+          />
+
+          <IonItem >
+            <IonText color="primary" >
+              Edit Picture
+          </IonText>
+          </IonItem>
+        </IonCard>
+
+        <IonLabel>Account Information</IonLabel>
+
+        <IonCard>
+          <IonList>
+            <IonItem>
+              <IonText color="primary" >
+                Change username
+          </IonText>
+            </IonItem>
+
+            <IonItem>
+              <IonText color="primary" >
+                Change Email
+          </IonText>
+            </IonItem>
+
+            <IonItem>
+              <IonText color="primary" >
+                Change Password
+          </IonText>
+            </IonItem>
+
+          </IonList>
+        </IonCard>
+        <IonLabel>Display</IonLabel>
+        <IonCard>
+          <IonItem>
+            <IonLabel> Switch Theme</IonLabel>
+            <IonToggle
+              slot="end"
+              name="darkMode"
+              onIonChange={toggleDarkModeHandler}
+            />
+          </IonItem>
+        </IonCard>
+
+        <IonButton
+          color="medium"
+          expand="block"
+          onClick={() => auth.signOut()}
+
+        >
+          Logout
+    </IonButton>
+
+        {/*  <IonModal
+      isOpen={showFaqModal}
+      onDidDismiss={() => setFaqModal(false)!}
+    >
+      <FaqPage turnOffModal={turnOffFaq} />
+    </IonModal>
+
+    <IonModal
+      isOpen={showCreditModal}
+      onDidDismiss={() => setCreditModal(false)!}
+    >
+      <CreditsPage turnOffModal={turnOffCredit} />
+    </IonModal> */}
+
+      </IonContent>
+
+    </IonPage>
+  );
+};
+
+
+
+
+{/* <IonPage>
 <IonHeader>
 <IonToolbar>
     <IonTitle>Settings</IonTitle>
@@ -30,13 +127,14 @@ const SettingsPage: React.FC = () => {
 </IonHeader>
 
 
-
-
-
-
-
-
-
+<IonItem>
+              <IonLabel> Switch Theme</IonLabel>
+              <IonToggle
+                slot="end"
+                name="darkMode"
+                onIonChange={toggleDarkModeHandler}
+              />
+            </IonItem>
 
 <IonContent className="ion-padding">
   <IonButton color="medium" expand="block"
@@ -46,5 +144,7 @@ const SettingsPage: React.FC = () => {
 </IonContent>
 </IonPage>
 );
-};
+}; */}
+
+
 export default SettingsPage;
